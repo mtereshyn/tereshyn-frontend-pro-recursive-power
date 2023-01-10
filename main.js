705da08d517ddb6e453be0c67b207fb705a8b437
Maksym Tereshyn <max.tereshyn@gmail.com>
2023-01-10 12:46:56 +0200

    done

---
+++ main.js
@@ -0,0 +1,16 @@
+function powNumber(number, pow) {
+
+    if(number < 0) {
+        return "Number can`t to be < 0";
+    } else if(number === 0) {
+        return 0;
+    }
+
+    if(pow === 0) {
+        return 1;
+    }
+
+    return pow > 0 ? number * powNumber(number, --pow) : 1 / (powNumber(number, Math.abs(pow)));
+}
+
+console.log(powNumber(5, 0));
